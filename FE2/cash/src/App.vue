9399f033f55219fce620bdf4e105ae0794497bc6
ekrasikov <eugene.krasikov@gmail.com>
2018-11-01 22:46:22 +0100

    (feat)refactored for vue components

---
+++ FE2/cash/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <section class="hero is-light is-fullheight" id="root">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-6 is-offset-3">
+                    <h3 class="title has-text-grey">
+                        Cash!
+                    </h3>
+                    <p class="subtitle has-text-grey">
+                        Simple expense app
+                    </p>
+                    <div class="box">
+                        <expense-form @form-submitted="addExpense"
+                                      :categories="this.loadExpenseCategories()">
+                        </expense-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+import ExpenseForm from './components/ExpenseForm.vue';
+import { HTTP } from './http_common.js'
+
+export default {
+  name: 'app',
+  components: {
+    ExpenseForm
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    addExpense(expense) {
+        if( expense.category == "" ) {
+            alert('Please select a category');
+            return;
+        }
+//            axios.post('/expenses', {
+//                amount: amount,
+//                category: category,
+//                date: date, //ISO format
+//                comment: comment,
+//                user: 'Masha'             
+//            })
+//            .then(function (response) {
+//                console.log(response);
+//            })
+//            .catch(function (error) {
+//                console.log(error);
+//            })
+        alert('You spent ' + expense.amount + ' EUR on ' + expense.date);
+    },
+    loadExpenseCategories() {
+      var result = [];
+      HTTP.get('/categories')
+      .then(function (response) {
+        response.data.forEach(function(item, index, array) {
+          result.push(item.name);
+        });
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+      return result;
+    }
+  }
+}
+</script>
+
+<style lang="css">
+    @import '../node_modules/bulma/css/bulma.css';
+</style>
---
+++ FE2/cash/src/components/ExpenseForm.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <form>
+            <div class="field">
+                <div class="control">
+                    <input class="input is-large" placeholder="0.00 EUR" autofocus="" v-model="amount">
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <div class="select is-large is-fullwidth">
+                        <select v-model="category">
+                            <option disabled value="">Please select a category</option>
+                            <option v-for="item in categories">
+                                {{ item }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <datepicker class="is-large" placeholder="Select date" v-model="date" 
+                        :config="{ dateFormat: 'd-m-Y'}">
+                    </datepicker>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <input class="input is-large" placeholder="Optional comment here" v-model="comment">
+                </div>
+            </div>
+
+            <button class="button is-block is-info is-large is-fullwidth"
+                @click="() => this.$emit('form-submitted', this)">
+                Submit
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+import Datepicker from 'vue-bulma-datepicker'
+
+export default {
+   components: {
+     Datepicker
+   },
+   props: [ "categories" ],
+   data() {
+       return {
+            amount: "",
+            category: "",
+            date: (new Date()).toString(),
+            comment: ""
+       }
+   },
+   methods: {
+        updated() {
+            // to add - if supermarket in categories, then it, otherwise the first item
+            this.category = this.categories[0];
+        }  
+   }
+}
+</script>
+
+<style>
+</style>
